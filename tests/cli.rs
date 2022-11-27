88fd7c28fe59c7fc68389ed204713f94023ae23a
Yareeque <jrslwlkn@gmail.com>
2022-11-26 20:22:08 -0600

    feat & test: set up end-to-end tests

---
+++ tests/cli.rs
@@ -0,0 +1,47 @@
+use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn hello_world() -> Result<(), Box<dyn std::error::Error>> {
+    let source = assert_fs::NamedTempFile::new("source.awk")?;
+    source.write_str("BEGIN { print 'hello', ',' 'world' }")?;
+
+    let mut cmd = Command::cargo_bin("yarawk")?;
+    cmd.arg("-f").arg(source.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("hello , world\n"));
+
+    Ok(())
+}
+
+#[test]
+fn actions() -> Result<(), Box<dyn std::error::Error>> {
+    let source = assert_fs::NamedTempFile::new("source.awk")?;
+    source.write_str(
+        r#"
+        { print }
+        END {
+            print FNR, NR
+        }
+    "#,
+    )?;
+
+    let in1 = assert_fs::NamedTempFile::new("in1.awk")?;
+    in1.write_str("hello\nworld\n");
+    let in2 = assert_fs::NamedTempFile::new("in2.awk")?;
+    in2.write_str("hello\nworld\n");
+
+    let mut cmd = Command::cargo_bin("yarawk")?;
+    cmd.arg("-f")
+        .arg(source.path())
+        .arg(in1.path())
+        .arg(in2.path());
+    cmd.assert().success().stdout(predicate::str::contains(
+        "hello\nworld\nhello\nworld\n2 4\n",
+    ));
+
+    Ok(())
+}
